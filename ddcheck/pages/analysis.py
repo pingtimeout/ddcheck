c59994b20045bbba56653d5284e098e6c3f6c954
Pierre Laporte <pierre@pingtimeout.fr>
2025-02-06 10:27:21 +0100

    Fix flake8 issue

--- ddcheck/pages/analysis.py
+++ ddcheck/pages/analysis.py
@@ -3,7 +3,7 @@ import streamlit as st
 if "ddcheck_id" in st.query_params:
     st.session_state["ddcheck_id"] = st.query_params["ddcheck_id"]
 
-if not "ddcheck_id" in st.session_state:
+if "ddcheck_id" not in st.session_state:
     st.switch_page("pages/01_Upload.py")
 
 # Add your analysis logic here
