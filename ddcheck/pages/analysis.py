ce81ef980a9bc2a1df14f8971017d60b19c546ed
Pierre Laporte <pierre@pingtimeout.fr>
2025-02-06 09:46:29 +0100

    feat: Add page switch for missing ddcheck_id

--- ddcheck/pages/analysis.py
+++ ddcheck/pages/analysis.py
@@ -3,5 +3,8 @@ import streamlit as st
 if "ddcheck_id" in st.query_params:
     st.session_state["ddcheck_id"] = st.query_params["ddcheck_id"]
 
+if not "ddcheck_id" in st.session_state:
+    st.switch_page("pages/01_Upload.py")
+
 # Add your analysis logic here
 st.write(f"Analyzing file: {st.session_state.ddcheck_id}")
