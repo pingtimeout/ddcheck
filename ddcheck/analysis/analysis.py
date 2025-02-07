90e0c4c8ce8604044bf2d38b900002be1a897c33
Pierre Laporte <pierre@pingtimeout.fr>
2025-02-07 14:45:48 +0100

    Update overall state after os info parsing

--- ddcheck/analysis/analysis.py
+++ ddcheck/analysis/analysis.py
@@ -1,3 +1,4 @@
+from ddcheck.analysis.osinfo import analyse_os_info
 from ddcheck.analysis.top import analyse_top_output
 from ddcheck.storage import AnalysisState, DdcheckMetadata
 from ddcheck.storage.upload import write_metadata_to_disk
@@ -6,7 +7,7 @@ from ddcheck.storage.upload import write_metadata_to_disk
 def analyse_tarball(metadata: DdcheckMetadata, node: str) -> AnalysisState:
     try:
         top_result = analyse_top_output(metadata, node)
-
-        return top_result
+        os_info_result = analyse_os_info(metadata, node)
+        return top_result.reduce_with(os_info_result)
     finally:
         write_metadata_to_disk(metadata)
